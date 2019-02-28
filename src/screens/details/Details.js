7bd9223bbcb5001a06146e16220524cdc854949f
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-02-28 16:00:49 +0530

    Added rating on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -9,13 +9,41 @@ import YouTube from "react-youtube"
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 class Details extends Component {
 
     constructor() {
         super();
         this.state = {
-            movie: []
+            movie: [],
+            starIcons: [
+                {
+                   id: 1,
+                   stateId: "star1",
+                   color: "black"
+                },
+                {
+                   id: 2,
+                   stateId: "star2",
+                   color: "black"
+                },
+                {
+                   id: 3,
+                   stateId: "star3",
+                   color: "black"
+                },
+                {
+                   id: 4,
+                   stateId: "star4",
+                   color: "black"
+                },
+                {
+                   id: 5,
+                   stateId: "star5",
+                   color: "black"
+                }
+             ]
         }
     }
 
@@ -31,8 +59,26 @@ class Details extends Component {
     artistClickHandler = (url) => {
         window.location = url;
     }
+
     backtohomeHandler = () => {
         ReactDOM.render(<Home />, document.getElementById('root'));
+    }
+
+    starClickHandler = (id) => {
+        let starIconList = [];
+        for (let star of this.state.starIcons){
+            let starNode = star;
+            if(star.id <= id){
+                starNode.color =  "yellow"
+            }
+            else {
+                starNode.color = "black"
+            }
+            starIconList.push(starNode)
+        }
+        this.setState({
+            starIcons: starIconList
+        });
     }
     render() {
         let movie = this.state.movie;
@@ -84,6 +130,15 @@ class Details extends Component {
                     </div>
                 </div>
                 <div className="rightDetails">
+                    <Typography> <span className="bold">Rate this movie: </span></Typography>
+                    {
+                        this.state.starIcons.map(star => (
+                            <StarBorderIcon 
+                                className={star.color} 
+                                key={"start"+ star.id}
+                                onClick={() => this.starClickHandler(star.id)}/>
+                        ))
+                    }
                     <div className="bold marginBottom16 marginTop16">
                         <Typography>
                             <span className="bold">Artists:</span>
