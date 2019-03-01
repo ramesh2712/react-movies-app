8556ec5851e18d2ac8a5a54d1dc24ce470271f9b
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-03-01 15:46:28 +0530

    Added unit price, total price and book show button on book show page

--- src/screens/bookshow/BookShow.js
+++ src/screens/bookshow/BookShow.js
@@ -15,6 +15,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import { Button } from '@material-ui/core';
 
 class BookShow extends Component {
 
@@ -24,7 +25,10 @@ class BookShow extends Component {
             location: "",
             language: "",
             showdate: "",
-            showtime: ""
+            showtime: "",
+            tickets: 0,
+            unitPrice: 500,
+            availableTickets: 20
         }
     }
 
@@ -33,16 +37,19 @@ class BookShow extends Component {
     }
 
     locationChangeHandler = (event) => {
-        this.setState({location: event.target.value})
+        this.setState({ location: event.target.value })
     }
     languageChangeHandler = (event) => {
-        this.setState({language: event.target.value})
+        this.setState({ language: event.target.value })
     }
     showDateChangeHandler = event => {
-        this.setState({showdate: event.target.value})
+        this.setState({ showdate: event.target.value })
     }
     showTimeChangeHandler = event => {
-        this.setState({showtime: event.target.value})
+        this.setState({ showtime: event.target.value })
+    }
+    ticketsChangeHandler = event => {
+        this.setState({ tickets: event.target.value })
     }
 
     render() {
@@ -106,6 +113,21 @@ class BookShow extends Component {
                                     ))}
                                 </Select>
                             </FormControl> <br /> <br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="tickets"> Tickets: ( {this.state.availableTickets} available) </InputLabel>
+                                <Input id="tickets"
+                                    value={this.state.tickets !== 0 ? this.state.tickets : ""}
+                                    onChange={this.ticketsChangeHandler} />
+                            </FormControl> <br /><br />
+                            <Typography>
+                                Unit Price: Rs. {this.state.unitPrice}
+                            </Typography> <br/><br/>
+                            <Typography>
+                                Total Price Rs. {this.state.unitPrice * this.state.tickets}
+                            </Typography><br/><br/>
+                            <Button variant="contained" 
+                                    onClick={this.bookShowButtonHandler}
+                                    color="primary"> BOOK SHOW </Button>
                         </CardContent>
                     </Card>
                 </div>
